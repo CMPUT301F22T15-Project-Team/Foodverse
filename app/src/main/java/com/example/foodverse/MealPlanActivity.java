1fcb5d8725242d9e35ea0f8ce664c90a428eb609
Cole Hoff <choff@ualberta.ca>
2022-10-29 15:26:26 -0600

    Fixed the ingredient spinner in the meal plan

--- app/src/main/java/com/example/foodverse/MealPlanActivity.java
+++ app/src/main/java/com/example/foodverse/MealPlanActivity.java
@@ -29,7 +29,7 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 
-public class MealPlanActivity extends AppCompatActivity implements MealPlanFragment.OnFragmentInteractionListener {
+public class MealPlanActivity extends AppCompatActivity implements MealTestFragment.OnFragmentInteractionListener {
 
     private ListView mealListView;
     private ArrayAdapter<Meal> mealAdapter;
@@ -45,10 +45,10 @@ public class MealPlanActivity extends AppCompatActivity implements MealPlanFragm
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_plan);
 
-        //mealArrayList = new ArrayList<>();
         selectedMealIndex = -1;
         mealListView = findViewById(R.id.meal_list);
 
+        // Initialize attributes
         mealArrayList = new ArrayList<>();
         mealAdapter = new MealList(this, mealArrayList);
         mealListView.setAdapter(mealAdapter);
@@ -91,7 +91,7 @@ public class MealPlanActivity extends AppCompatActivity implements MealPlanFragm
         addMealButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new MealPlanFragment().show(getSupportFragmentManager(), "TEST");
+                new MealTestFragment().show(getSupportFragmentManager(), "TEST");
             }
         });
 
@@ -102,7 +102,7 @@ public class MealPlanActivity extends AppCompatActivity implements MealPlanFragm
                                             int position, long id) {
                         Meal meal = mealAdapter.getItem(position);
                         selectedMealIndex = position;
-                        new MealPlanFragment(meal).show(
+                        new MealTestFragment(meal).show(
                                 getSupportFragmentManager(), "EDIT_MEAL");
                     }
                 });
