8f26d13189e9f866427d2e004ec03b45c8c8870f
Tyler Sadler <tsadler@ualberta.ca>
2022-10-31 16:38:17 -0600

    Updated documentation for DatabaseIngredient

--- app/src/main/java/com/example/foodverse/DatabaseIngredient.java
+++ app/src/main/java/com/example/foodverse/DatabaseIngredient.java
@@ -1,37 +1,47 @@
 package com.example.foodverse;
 
-import android.provider.ContactsContract;
 
 import androidx.annotation.NonNull;
 
+
 /**
- * A simple static class providing extra functionality to Ingredients that make
- * it far easier to store them in the Firestore database.
+ * A simple static class providing extra functionality to the {@link Ingredient}
+ * class to convert them to {@link String} objects, or to convert a
+ * {@link String} back to an {@link Ingredient}. This makes it far easier to
+ * store a list of {@link Ingredient} objects in the Firestore database.
  *
- * @Version 1.0
  * @author Tyler
+ * @version 1.0
  */
 public class DatabaseIngredient {
     /**
-     * toString used to store ingredients as strings so they can easily
-     * be reconstructed upon retrieval from database.
+     * A method used to take the data from a {@link Ingredient} and store it in
+     * a {@link String}, with fields separated by "|".
      *
+     * @param ingredient A {@link Ingredient} object to convert to a string
+     *                   representation.
      * @return ingString, a {@link String} representing the {@link Ingredient}.
+     * @since 1.0
      */
-    public static String ingredientToString(Ingredient ingredient) {
+    @NonNull
+    public static String ingredientToString(@NonNull Ingredient ingredient) {
         String ingString = ingredient.getDescription();
         ingString += "|" + String.valueOf(ingredient.getCount());
         return ingString;
     }
 
+
     /**
      * A method used to extract data from a {@link String} and return a new
      * {@link Ingredient} object constructed from its contents.
+     *
      * @param ingString A {@link String} representing an {@link Ingredient}
      *                  object.
      * @return A new {@link Ingredient} object constructed from the string.
+     * @since 1.0
      */
-    public static Ingredient stringToIngredient(String ingString) {
+    @NonNull
+    public static Ingredient stringToIngredient(@NonNull String ingString) {
         /*
          * https://stackoverflow.com/questions/7683448/in-java-how-to-get-substring-from-a-string-till-a-character-c
          * For nicely splitting the string. Answer by Chad Schouggins (2011)
