7d15305aa15e0a91baa9b4cc7bbf747f96ddb47f
Cole Hoff <choff@ualberta.ca>
2022-11-18 21:30:30 -0700

    Improved how the dates are displayed for meals

--- app/src/main/java/com/example/foodverse/MealList.java
+++ app/src/main/java/com/example/foodverse/MealList.java
@@ -10,7 +10,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * An array adapter to allow for meals to be displayed in a listview.
@@ -50,7 +54,27 @@ public class MealList extends ArrayAdapter<Meal> {
         TextView mealServings = view.findViewById(R.id.meal_servings);
 
 
-        mealDate.setText(meal.getDate().toString());
+        //mealDate.setText(meal.getDate().toString());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(meal.getDate());
+        //calendar.set(Calendar.HOUR_OF_DAY, 0);
+        //calendar.set(Calendar.MINUTE, 0);
+        //calendar.set(Calendar.SECOND, 0);
+        //calendar.set(Calendar.MILLISECOND, 0);
+        String dayString = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.CANADA);
+        String monthString = calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.CANADA);
+        //String numString = calendar.getDisplayName(Calendar.DAY_OF_MONTH, Calendar.LONG, Locale.CANADA);
+        //String numString = Integer(calendar.get(Calendar.DAY_OF_YEAR)).toString();
+        int dateInt = calendar.get(Calendar.DAY_OF_MONTH);
+        Integer toStringInt = new Integer(dateInt);
+        mealDate.setText(dayString + " " + monthString + " " + toStringInt.toString());
+
+        //mealDate.setText(calendar.getTime().toString());
+        String dateString = meal.getDate().toString();
+
+        //SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        //Date cleanDate = dateFormat.parse(dateFormat.format(meal.getDate()));
+        //mealDate.setText(calendar.toString());
         //mealName.setText(meal.get);
         //mealServings.setText((int) meal.getNumberOfServings());
 
